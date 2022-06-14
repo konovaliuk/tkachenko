7583772a171393f4e9fbf8522b97b494004e0779
Maksym Tkachenko <maksym_tkachenko2@epam.com>
2022-06-14 10:02:17 +0300

    Lab_1

--- service-claims/src/main/java/com/chessplatform/serviceclaims/repo/model/ReportStatus.java
+++
@@ -1,1 +0,0 @@
-package com.chessplatform.serviceclaims.repo.model;
--- service-game/src/main/java/com/chessplatform/servicegame/repo/GameRepo.java
+++
@@ -1,7 +0,0 @@
-package com.chessplatform.servicegame.repo;
-
-import com.chessplatform.servicegame.repo.model.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepo extends JpaRepository<Game, Long> {
-}
